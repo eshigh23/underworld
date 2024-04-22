3f61f46cd9dec300f02d1b742a1e4ac763382ae2
lily <lshigh23@gmail.com>
2024-04-21 22:18:07 -0400

    second commit

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,9 @@ export default function App(){
 
   const [backendData, setBackendData] = useState([{}])
 
-  // defined proxy address (localhost:5000) in client package.json, letting us use relative address
+  /* fetch (dummy) info from server (server.js) and update backendData var with the data
+  defined proxy address (localhost:5000) in client package.json, letting us use relative address
+  */
   useEffect(() => {
     fetch("/api").then(
       response => response.json()
@@ -14,6 +16,7 @@ export default function App(){
       }
     )
   }, [])
+
 
   return(
     <div>
